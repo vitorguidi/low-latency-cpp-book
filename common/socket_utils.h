a26056d40661f5e3e20836aa52b554cf53c3c76d
Vitor Guidi <vitorguidi@gmail.com>
2026-06-08 20:01:08 -0700

    Fix socket_utils.h and write tcp_socket.h

--- common/socket_utils.h
+++ common/socket_utils.h
@@ -3,7 +3,6 @@
 #include <iostream>
 #include <string>
 #include <unordered_set>
-#include <sstream>
 #include <sys/epoll.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -20,27 +19,6 @@
 #include "logger.h"
 
 namespace Common {
-    struct SocketCfg {
-        std::string ip_;
-        std::string iface_;
-        int port_ = -1;
-        bool is_udp_ = false;
-        bool is_listening_ = false;
-        bool needs_so_timestamp_ = false;
-
-        auto toString() const {
-            std::stringstream ss;
-            ss << "SocketCfg[ip:" << ip_
-               << " iface:" << iface_
-               << " port:" << port_
-               << " is_udp:" << is_udp_
-               << " is_listening:" << is_listening_
-               << " needs_SO_timestamp:" << needs_so_timestamp_
-               << "]";
-            return ss.str();
-        }
-    };
-
     constexpr int MaxTCPServerBacklog = 1024;
 
     inline auto getIfaceIP(const std::string &iface) -> std::string {
@@ -62,12 +40,14 @@ namespace Common {
 
     inline auto setNonBlocking(int fd) -> bool {
         const auto flags = fcntl(fd, F_GETFL, 0);
+        if (flags == -1)
+            return false;
         if (flags & O_NONBLOCK)
             return true;
         return (fcntl(fd, F_SETFL, flags | O_NONBLOCK) != -1);
     }
 
-    inline auto disableNagle(int fd) -> bool {
+    inline auto setNoDelay(int fd) -> bool {
         int one = 1;
         return (setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, reinterpret_cast<void *>(&one), sizeof(one)) != -1);
     }
@@ -77,67 +57,110 @@ namespace Common {
         return (setsockopt(fd, SOL_SOCKET, SO_TIMESTAMP, reinterpret_cast<void *>(&one), sizeof(one)) != -1);
     }
 
-    inline auto join(int fd, const std::string &ip) -> bool {
+    inline auto wouldBlock() -> bool {
+        return (errno == EWOULDBLOCK || errno == EINPROGRESS);
+    }
+
+    inline auto setMcastTTL(int fd, int mcast_ttl) noexcept -> bool {
+        return (setsockopt(fd, IPPROTO_IP, IP_MULTICAST_TTL, reinterpret_cast<void *>(&mcast_ttl), sizeof(mcast_ttl)) != -1);
+    }
+
+    inline auto setTTL(int fd, int ttl) -> bool {
+        return (setsockopt(fd, IPPROTO_IP, IP_TTL, reinterpret_cast<void *>(&ttl), sizeof(ttl)) != -1);
+    }
+
+    inline auto join(int fd, const std::string &ip, const std::string &iface, int port) -> bool {
         const ip_mreq mreq{{inet_addr(ip.c_str())}, {htonl(INADDR_ANY)}};
         return (setsockopt(fd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) != -1);
     }
 
-    [[nodiscard]] inline auto createSocket(Logger &logger, const SocketCfg &socket_cfg) -> int {
+    inline auto createSocket(Logger &logger, const std::string &t_ip, const std::string &iface,
+                             int port, bool is_udp, bool is_blocking, bool is_listening,
+                             int ttl, bool needs_so_timestamp) -> int {
         std::string time_str;
+        const auto ip = t_ip.empty() ? getIfaceIP(iface) : t_ip;
+        logger.log("%:% %() % ip:% iface:% port:% is_udp:% is_blocking:% is_listening:% ttl:% SO_time:%\n",
+                   __FILE__, __LINE__, __FUNCTION__,
+                   Common::getCurrentTimeStr(&time_str), ip, iface, port, is_udp, is_blocking,
+                   is_listening, ttl, needs_so_timestamp);
 
-        const auto ip = socket_cfg.ip_.empty() ? getIfaceIP(socket_cfg.iface_) : socket_cfg.ip_;
-        logger.log("%:% %() % cfg:%\n", __FILE__, __LINE__, __FUNCTION__,
-                   Common::getCurrentTimeStr(&time_str), socket_cfg.toString());
-
-        const int input_flags = (socket_cfg.is_listening_ ? AI_PASSIVE : 0) | (AI_NUMERICHOST | AI_NUMERICSERV);
-        const addrinfo hints{input_flags, AF_INET, socket_cfg.is_udp_ ? SOCK_DGRAM : SOCK_STREAM,
-                             socket_cfg.is_udp_ ? IPPROTO_UDP : IPPROTO_TCP, 0, 0, nullptr, nullptr};
+        addrinfo hints{};
+        hints.ai_family = AF_INET;
+        hints.ai_socktype = is_udp ? SOCK_DGRAM : SOCK_STREAM;
+        hints.ai_protocol = is_udp ? IPPROTO_UDP : IPPROTO_TCP;
+        hints.ai_flags = is_listening ? AI_PASSIVE : 0;
+        if (std::isdigit(ip.c_str()[0]))
+            hints.ai_flags |= AI_NUMERICHOST;
+        hints.ai_flags |= AI_NUMERICSERV;
 
         addrinfo *result = nullptr;
-        const auto rc = getaddrinfo(ip.c_str(), std::to_string(socket_cfg.port_).c_str(), &hints, &result);
-        ASSERT(!rc, "getaddrinfo() failed. error:" + std::string(gai_strerror(rc)) + "errno:" + strerror(errno));
+        const auto rc = getaddrinfo(ip.c_str(), std::to_string(port).c_str(), &hints, &result);
+        if (rc) {
+            logger.log("getaddrinfo() failed. error:% errno:%\n", gai_strerror(rc), strerror(errno));
+            return -1;
+        }
 
-        int socket_fd = -1;
+        int fd = -1;
         int one = 1;
         for (addrinfo *rp = result; rp; rp = rp->ai_next) {
-            ASSERT((socket_fd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol)) != -1,
-                   "socket() failed. errno:" + std::string(strerror(errno)));
-
-            ASSERT(setNonBlocking(socket_fd),
-                   "setNonBlocking() failed. errno:" + std::string(strerror(errno)));
-
-            if (!socket_cfg.is_udp_) {
-                ASSERT(disableNagle(socket_fd),
-                       "disableNagle() failed. errno:" + std::string(strerror(errno)));
+            fd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+            if (fd == -1) {
+                logger.log("socket() failed. errno:%\n", strerror(errno));
+                return -1;
             }
 
-            if (!socket_cfg.is_listening_) {
-                ASSERT(connect(socket_fd, rp->ai_addr, rp->ai_addrlen) != 1,
-                       "connect() failed. errno:" + std::string(strerror(errno)));
+            if (!is_blocking) {
+                if (!setNonBlocking(fd)) {
+                    logger.log("setNonBlocking() failed. errno:%\n", strerror(errno));
+                    return -1;
+                }
+                if (!is_udp && !setNoDelay(fd)) {
+                    logger.log("setNoDelay() failed. errno:%\n", strerror(errno));
+                    return -1;
+                }
             }
 
-            if (socket_cfg.is_listening_) {
-                ASSERT(setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, reinterpret_cast<const char *>(&one), sizeof(one)) == 0,
-                       "setsockopt() SO_REUSEADDR failed. errno:" + std::string(strerror(errno)));
+            if (!is_listening && connect(fd, rp->ai_addr, rp->ai_addrlen) == 1 && !wouldBlock()) {
+                logger.log("connect() failed. errno:%\n", strerror(errno));
+                return -1;
             }
 
-            if (socket_cfg.is_listening_) {
-                const sockaddr_in addr{AF_INET, htons(socket_cfg.port_), {htonl(INADDR_ANY)}, {}};
-                ASSERT(bind(socket_fd, socket_cfg.is_udp_ ? reinterpret_cast<const struct sockaddr *>(&addr) : rp->ai_addr, sizeof(addr)) == 0,
-                       "bind() failed. errno:%" + std::string(strerror(errno)));
+            if (is_listening && setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, reinterpret_cast<const char *>(&one), sizeof(one)) == -1) {
+                logger.log("setsockopt() SO_REUSEADDR failed. errno:%\n", strerror(errno));
+                return -1;
             }
 
-            if (!socket_cfg.is_udp_ && socket_cfg.is_listening_) {
-                ASSERT(listen(socket_fd, MaxTCPServerBacklog) == 0,
-                       "listen() failed. errno:" + std::string(strerror(errno)));
+            if (is_listening && bind(fd, rp->ai_addr, rp->ai_addrlen) == -1) {
+                logger.log("bind() failed. errno:%\n", strerror(errno));
+                return -1;
             }
 
-            if (socket_cfg.needs_so_timestamp_) {
-                ASSERT(setSOTimestamp(socket_fd),
-                       "setSOTimestamp() failed. errno:" + std::string(strerror(errno)));
+            if (!is_udp && is_listening && listen(fd, MaxTCPServerBacklog) == -1) {
+                logger.log("listen() failed. errno:%\n", strerror(errno));
+                return -1;
+            }
+
+            if (is_udp && ttl) {
+                const bool is_multicast = atoi(ip.c_str()) & 0xe0;
+                if (is_multicast && !setMcastTTL(fd, ttl)) {
+                    logger.log("setMcastTTL() failed. errno:%\n", strerror(errno));
+                    return -1;
+                }
+                if (!is_multicast && !setTTL(fd, ttl)) {
+                    logger.log("setTTL() failed. errno:%\n", strerror(errno));
+                    return -1;
+                }
+            }
+
+            if (needs_so_timestamp && !setSOTimestamp(fd)) {
+                logger.log("setSOTimestamp() failed. errno:%\n", strerror(errno));
+                return -1;
             }
         }
 
-        return socket_fd;
+        if (result)
+            freeaddrinfo(result);
+
+        return fd;
     }
 }
